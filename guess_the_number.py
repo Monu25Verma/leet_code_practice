18b406a4db5ad995fac58a6d61643644841474e4
Monu25Verma <monuv2397@gmail.com>
2023-08-08 21:54:10 +0530

    code refactor(the process of restructuring code while not changing its original functionality)

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,32 +3,37 @@
 # mid = f"I am a girl {name:}, i work in {company_name:} company as a system engineer"
 # print(mid)
 import random
+
+
 def guess(x):
     random_val = random.randint(1, x)
-    guess = 0
-    while guess != random_val:
-        guess = int(input(f"enter the number between 1 to {x}: "))
-        if guess < random_val:
+    guess_val = 0
+    while guess_val != random_val:
+        guess_val = int(input(f"enter the number between 1 to {x}: "))
+        if guess_val < random_val:
             print("the number is to low")
-        elif guess > random_val:
+        elif guess_val > random_val:
             print("the number is too high")
     print("congrats guess number is correct:", random_val)
+
 
 def guess_computer(x):
     low = 1
     high = x
     feedback = ''
+    guess_val = 0
     while feedback != 'c':
         if low != high:
-            guess = random.randint(low, high)
+            guess_val = random.randint(low, high)
         else:
-            guess = low
-        feedback = input(f'whet is your choice {guess}  (H),it is too high, (L) too low, (C) correct" : ')
+            guess_val = low
+        feedback = input(f'whet is your choice {guess_val}  (H),it is too high, (L) too low, (C) correct" : ')
         if feedback == 'h':
-            guess = high - 1
+            guess_val = high - 1
         elif feedback == 'l':
-            guess = low + 1
-    print(f'yat! congratulation computer has guess  the correct number {guess}')
+            guess_val = low + 1
+    print(f'yat! congratulation computer has guess  the correct number {guess_val}')
+
 
 guess_computer(20)
 
