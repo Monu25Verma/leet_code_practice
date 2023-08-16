ad0bcb75efa5e0c2a66e92d4749e48466a8f1569
Monu25Verma <monuv2397@gmail.com>
2023-08-16 22:13:18 +0530

    257. Binary Tree Paths leetcode

--- binary_tree_path.py
+++ binary_tree_path.py
@@ -1,8 +1,15 @@
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# 257. Binary Tree Paths
+
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def binaryTreePaths(root: Optional[TreeNode]) -> List[str]:
 
@@ -20,4 +27,6 @@ class Solution:
 
         DFS(root, "")
         return result
-print(Solution.binaryTreePaths([6,2,9,1,7,0])
+
+tree = TreeNode(1,TreeNode(2, None,TreeNode(5,None, None)),TreeNode(3,None, None))
+print(Solution.binaryTreePaths(tree))
