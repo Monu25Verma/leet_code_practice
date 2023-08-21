c77989a229181a371bc039400df11d47a86c4e7f
Monu25Verma <monuv2397@gmail.com>
2023-08-21 20:04:31 +0530

    leetcode practice

--- 283_Move_Zeroes.py
+++ 283_Move_Zeroes.py
@@ -1,19 +1,37 @@
-from typing import List
-
-
+# from typing import List
+#
+#
+# class Solution:
+#     def moveZeroes(nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         i = 0
+#         n = len(nums)
+#         for j in range(n):
+#             if nums[j] != 0:
+#                 nums[i], nums[j] = nums[j], nums[i]
+#                 i += 1
+#
+#
+#
+# print(Solution.moveZeroes([0,1,0,3,12]))
+#
 class Solution:
-    def moveZeroes(nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        i = 0
-        n = len(nums)
-        for j in range(n):
-            if nums[j] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
-                i += 1
+    def wordPattern(pattern: str, s: str) -> bool:
+        res = {}
+        s = s.split()
+        if len(pattern) != len(s):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] not in res.keys():
+                if s[i] not in res.values():
+                    res[pattern[i]] = s[i]
+                else:
+                    return False
+            elif res[pattern[i]] != s[i]:
+                return False
+        return True
 
-
-
-print(Solution.moveZeroes([0,1,0,3,12]))
-
+print(Solution.wordPattern("abba", "dog cat cat dog"))
+        
