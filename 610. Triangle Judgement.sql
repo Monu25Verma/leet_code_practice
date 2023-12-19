46e89f60f1e010a194251065503452200b04b0e2
Monu25Verma <monuv2397@gmail.com>
2023-12-19 16:47:42 +0530

    leetcode practice

---
+++ 610. Triangle Judgement.sql
@@ -0,0 +1,3 @@
+select  *,  IF (x+y > z and y+z>x and z+x>y, "Yes", "No") as triangle from Triangle;
+
+# * - is the multiply uses to display all the column
---
+++ 619. Biggest Single Number.sql
@@ -0,0 +1,12 @@
+
+#A single number is a number that appeared only once in the MyNumbers table.
+#Find the largest single number. If there is no single number, report null.
+
+
+select max(num) as num
+   from (
+       select num
+       from MyNumbers
+       group by num
+        having count(num) = 1
+) AS unique_number;
