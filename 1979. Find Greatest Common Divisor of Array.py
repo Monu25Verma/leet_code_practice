d5f1f68aeee81debe0f116d62393cfa3257178f1
Monu25Verma <monuv2397@gmail.com>
2023-12-25 13:08:18 +0530

    1979 gcd to find common divisor

--- 1979. Find Greatest Common Divisor of Array.py
+++ 1979. Find Greatest Common Divisor of Array.py
@@ -1,18 +1,19 @@
 class Solution:
-    def find_div(num):
+    def find_div(num: int):
         result1 = []
         for i in range(1, num + 1):
             if num % i == 0:
                 result1.append(i)
         return result1
 
-    def findGCD(self, nums: List[int]) -> int:
+    def findGCD(nums: list[int]) -> int:
 
         s = sorted(nums)
         small_val = s[0]  # 3
         large_val = s[-1]  # 8
-        result_s = find_div(small_val)
-        result_l = find_div(large_val)
+        result_s = Solution.find_div(small_val)
+        result_l = Solution.find_div(large_val)
+
         if len(result_s) > len(result_l):
             max_list = result_l
             min_list = result_s
@@ -25,3 +26,6 @@ class Solution:
             if i in min_list:
                 result.append(i)
         return max(sorted(result))
+
+
+print(Solution.findGCD([2, 5, 6, 9, 10]))
