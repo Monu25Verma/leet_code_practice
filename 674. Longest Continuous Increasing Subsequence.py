3ada1df7e406582bdb56b9cab1d7f3ed18ada4ef
Monu25Verma <monuv2397@gmail.com>
2024-01-05 20:46:13 +0530

    674 longest continuous subquery

--- 674. Longest Continuous Increasing Subsequence.py
+++ 674. Longest Continuous Increasing Subsequence.py
@@ -1,9 +1,11 @@
 import operator
+class Solution:
+    def findLengthOfLCIS(nums: list[int]) -> int:
+        dp = [1] * len(nums)  # [1,1,1,1,1]
+        for i in range(1, len(nums)):  # (1, 5)
+            if nums[i] > nums[i - 1]:  # 3 > 2
+                dp[i] += dp[i - 1]  #
+        return max(dp)
 
-nums = [1,3,5,4,7]
-dp = [1] * len(nums)  # [1,1,1,1,1]
-for i in range(1, len(nums)):  # (1, 5)
-    if nums[i] > nums[i - 1]:  # 3 > 2
-        dp[i] += dp[i - 1]  #
-print(max(dp))
 
+print(Solution.findLengthOfLCIS([1, 3, 5, 4, 7]))
